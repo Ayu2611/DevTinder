bf3cffcae8c23c5eb040fa4da089725904b169ad
Ayu2611 <ayushichouhan2611@gmail.com>
2025-09-01 17:35:04 +0530

    http

--- src/app.js
+++ src/app.js
@@ -6,13 +6,20 @@ const app = express();
 res.send("helo from new route")
 })*/
 
-
-app.use("/",(req,res)=>{
-res.send("be.")
-})
 /*app.use((req,res)=>{
     res.send("helloo")
 }) */
+app.get("/user",(req,res)=>{
+    res.send("user route using get method")
+})
+app.post("/user",(req,res)=>{
+    res.send("user sending data using post method")
+})
+app.delete("/user",(req,res)=>{
+    res.send("user deleted using dlete route method")
+})
+
+
 
 app.listen(3000,()=>{
     console.log("running")
